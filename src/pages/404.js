9815393538044d304ebc739150499b0f27520763
AndrewKirpanev <spacewebdeveloper@gmail.com>
2020-07-24 21:39:42 +1000

    Fix build

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import LayoutMain from 'layouts/layout-main';
-import SEO from 'components/shared/seo/seo';
 
 const NotFoundPage = () => (
   <LayoutMain>
-    <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadaaaaaness.</p>
   </LayoutMain>
